07e91d30240ae34e1aa46a162e84cfd64b564307
Anon <Anon@192.168.56.1>
2021-03-28 17:53:20 +0500

    Иправление ошибок

--- src/main/java/laba4/Calc.java
+++ src/main/java/laba4/Calc.java
@@ -38,8 +38,8 @@ public class Calc extends HttpServlet {
 		public void setAsRequestAttributesAndCalculate(HttpServletRequest request) {
 			request.setAttribute("first_result", first_calc);
 			request.setAttribute("second_result", second_calc);
-			double first_try;
-			double second_try;
+			double first_try = 0;
+			double second_try = 0;
 			try { 
 			first_try=Integer.parseInt(first_calc);
 			second_try=Integer.parseInt(second_calc);
