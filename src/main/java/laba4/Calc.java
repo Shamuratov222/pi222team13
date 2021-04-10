ca695964e96ab503b25c8a36bda87a580f1c7124
Kolesova <lera3262@gmail.com>
2021-04-10 11:24:30 +0500

    Внесены изменения, сделан расчёт в мм

--- src/main/java/laba4/Calc.java
+++ src/main/java/laba4/Calc.java
@@ -49,7 +49,7 @@ public class Calc extends HttpServlet {
 				second_try=0;	
 			}
 			
-			result=first_try*second_try*0.5;
+			result=(first_try*second_try*0.5)*10;
 			request.setAttribute("result", result);
 		}}
 		
